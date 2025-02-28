3f6db43100a80ffd1b1e3a15be02b1c9184bddd4
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-28 11:42:47 +0100

    [feat] create array with language for api request

---
+++ src/data/language.js
@@ -0,0 +1,14 @@
+const lang = {
+    it: 'language=it_IT',
+    en: 'language=en_US',
+    es: 'language=es_ES',
+    fr: 'language=fr_FR',
+    de: 'language=de_DE',
+    pt: 'language=pt_PT',
+    ru: 'language=ru_RU',
+    ja: 'language=ja_JP',
+    ko: 'language=ko_KR',
+    zh: 'language=zh_CN'
+};
+
+export default lang;
