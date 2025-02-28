98566de8063eef21dae2ce08c94b37b54c34f97c
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-28 16:30:21 +0100

    [feat] logic for empty stars

--- src/utils/Functions/StarFs.jsx
+++ src/utils/Functions/StarFs.jsx
@@ -2,8 +2,9 @@
 export default function Star(num) {
     // dividi il numero totale di valutazione di 2 e arrotondalo
     let voto = Math.round(num / 2)
+    let empty = 5 - voto
     // salva la variabile che conterrà le stelle inizialmente vuota
-    let stars = []
+    let stars = [];
     // se il voto è diverso da 0 
     if (voto != 0) {
         // effettuiamo un ciclo con lunghezza voti
@@ -11,10 +12,16 @@ export default function Star(num) {
             // pusciamo all'interno del nostro array le stelline per quante volte viene effettuato il ciclo
             stars.push(<i key={i} className="fa-solid fa-star"></i>)
         }
+        for (let i = 0; i < empty; i++) {
+            // pusciamo all'interno del nostro array le stelline per quante volte viene effettuato il ciclo
+            stars.push(<i key={voto + i} className="fa-regular fa-star"></i>)
+        }
         // se il voto non è stato dato o è uguale a 0 
-    } else (
-        // rilasciamo un testo con scritto nessun voto
-        stars = <p>Nessun Voto</p>
-    )
+    } else {
+        for (let i = 0; i < empty; i++) {
+            // pusciamo all'interno del nostro array le stelline per quante volte viene effettuato il ciclo
+            stars.push(<i key={i} className="fa-regular fa-star"></i>)
+        }
+    }
     return stars
 }
