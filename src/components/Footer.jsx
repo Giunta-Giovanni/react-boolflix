1e3a9bd84847a287431d3078e3097444743da1eb
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-02 16:42:36 +0100

    [feat] social in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,47 @@ export default function Footer() {
     return (
         <footer>
             < div className="container" >
-                questo è il footer
+
+                {/* social link */}
+                <div className="box-channel">
+
+                    <ul className="channel">
+                        <li className="single-channel">
+                            <a href="#">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <i class="fa-brands fa-x-twitter"></i>
+                            </a>
+
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <i class="fa-brands fa-youtube"></i>
+                            </a>
+
+                        </li>
+
+                    </ul>
+                </div>
+
+                {/* member footer links */}
+
+
+
+                {/* member footer service */}
+
+                {/* member footer copyright */}
+
 
             </div >
         </footer>
