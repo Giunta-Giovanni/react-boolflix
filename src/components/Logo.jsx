ec795ed54ee0baa5a84cb0ec937eb50ba8e1448f
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-02 16:19:02 +0100

    [feat] link logo to homepage

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export default function Logo() {
     return (
         <>
-            <Link>
+            <Link to={'/'}>
                 <div className="logo">
                     <img src="../../public/img/logo/boolflix-extended-logo.png" alt="" />
                 </div>
