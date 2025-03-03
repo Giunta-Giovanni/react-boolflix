84026f632ec04fff257ec66e4119fd6bf1cd477a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-03 23:47:51 +0100

    [faet] test image slider in popular

--- src/pages/NuoviPopolariPage.jsx
+++ src/pages/NuoviPopolariPage.jsx
@@ -1,7 +1,22 @@
+import { useContext } from "react"
+import MediaContext from '../contexts/MediaContext'
+
+const urlRadiceimg = 'https://image.tmdb.org/t/p/w342/'
+
 export default function NuoviPopolariPage() {
+
+    const { film } = useContext(MediaContext);
+    console.log('film in nuovi popolari page', film)
+
     return (
-        <div>
-            questa è la mia NuoviPopolariPage
+        <div className="p-banner">
+            <div className="p-slider" style={{ "--quantity": film.length }}>
+                {film.map((film, index) => (
+                    <div className="p-item" key={index} style={{ "--position": `${index + 1}` }}>
+                        <img src={`${urlRadiceimg}/${film.poster_path}`} alt={film.title} />
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
